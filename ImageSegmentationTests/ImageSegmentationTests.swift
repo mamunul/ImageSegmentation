efd18470f57c26cae7f3df7cbfd37045f7fb0328
Mamunul Mazid <mamunul.mazid@gmail.com>
2020-01-22 14:54:01 +0600

    Initial Commit

---
+++ ImageSegmentationTests/ImageSegmentationTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImageSegmentationTests.swift
+//  ImageSegmentationTests
+//
+//  Created by New User on 22/1/20.
+//  Copyright © 2020 New User. All rights reserved.
+//
+
+import XCTest
+@testable import ImageSegmentation
+
+class ImageSegmentationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
